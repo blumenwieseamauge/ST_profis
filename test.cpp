4b5503e47a51ff69e77b06462deba25f97c91f00
Luisa von Wolffersdorff <luisa.vw@web.de>
2021-01-08 12:26:24 +0100

    test.cpp wurde überarbeitet Zwei Outputs wurden hinzugefügt

--- test.cpp
+++ test.cpp
@@ -7,5 +7,6 @@ int main(){
     for(int i = 0; i < 13; i++) std::cout << '#';
     std::cout << std::endl;
     std::cout << "Hallo Miss Gitty!" << std::endl;
+    std::cout << "Hallo Mrs Gitters!" << std::endl;
     return 0;
 }
