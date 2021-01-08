ce3b948e074740488f36ac451c345fbeab9f5cd0
Luisa von Wolffersdorff <luisa.vw@web.de>
2021-01-08 12:24:42 +0100

    C++ Datei test.cpp überarbeitet

--- test.cpp
+++ test.cpp
@@ -6,5 +6,6 @@ int main(){
     std::cout << "Hallo Mr Git!" << std::endl;
     for(int i = 0; i < 13; i++) std::cout << '#';
     std::cout << std::endl;
+    std::cout << "Hallo Miss Gitty!" << std::endl;
     return 0;
 }
