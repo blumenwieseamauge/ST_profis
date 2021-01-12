9a628ed072951fc1c16701cf78bbd30597ae5449
C1aas <55586721+C1aas@users.noreply.github.com>
2021-01-12 14:29:23 +0100

    Update test.cpp
    deleted additions to be able to clone before changes

--- test.cpp
+++ test.cpp
@@ -6,7 +6,5 @@ int main(){
     std::cout << "Hallo Mr Git!" << std::endl;
     for(int i = 0; i < 13; i++) std::cout << '#';
     std::cout << std::endl;
-    std::cout << "Hallo Miss Gitty!" << std::endl;
-    std::cout << "Hallo Mrs Gitters!" << std::endl;
     return 0;
 }
