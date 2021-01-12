b4370f778ad21339504971ca824da12edcf256ec
C1aas <55586721+C1aas@users.noreply.github.com>
2021-01-12 14:30:19 +0100

    Update test.cpp
    Add the addditition again

--- test.cpp
+++ test.cpp
@@ -6,5 +6,7 @@ int main(){
     std::cout << "Hallo Mr Git!" << std::endl;
     for(int i = 0; i < 13; i++) std::cout << '#';
     std::cout << std::endl;
+    std::cout << "Hallo Miss Gitty!" << std::endl;
+    std::cout << "Hallo Mrs Gitters!" << std::endl;
     return 0;
 }
