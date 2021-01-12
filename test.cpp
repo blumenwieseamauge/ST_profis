ded6e1fe7a45e7f54cc267254268fb5e8d953736
C1aas <claas.kochanke@gmail.com>
2021-01-12 14:35:24 +0100

    test.cpp bearbeitet und Asugabe hinzugefügt

--- test.cpp
+++ test.cpp
@@ -6,6 +6,7 @@ int main(){
     std::cout << "Hallo Mr Git!" << std::endl;
     for(int i = 0; i < 13; i++) std::cout << '#';
     std::cout << std::endl;
+    std::cout << "Claas ist der coolste!" << std::endl;
     std::cout << "Hallo Miss Gitty!" << std::endl;
     std::cout << "Hallo Mrs Gitters!" << std::endl;
     return 0;
