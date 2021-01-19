824231b0baefcab628f2bb890a55ed8585b071c1
C1aas <claas.kochanke@gmail.com>
2021-01-19 12:46:55 +0100

    änderung

--- test.cpp
+++ test.cpp
@@ -3,11 +3,11 @@
 int main(){
     for(int i = 0; i < 13; i++) std::cout << '#';
     std::cout << std::endl;
-    std::cout << "Hallo Mr Git!" << std::endl;
+    std::cout << "Hallo Mr Pringle!" << std::endl;
     for(int i = 0; i < 13; i++) std::cout << '#';
     std::cout << std::endl;
-    std::cout << "Claas ist der coolste!" << std::endl;
+    std::cout << "Claas ist immernoch der coolste!" << std::endl;
     std::cout << "Hallo Miss Gitty!" << std::endl;
-    std::cout << "Hallo Mrs Gitters!" << std::endl;
+    std::cout << "Hallo Timon du schnitte!" << std::endl;
     return 0;
 }
